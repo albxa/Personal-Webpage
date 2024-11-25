11aeadb8052dc7ab792e80c0fff3d7e5b9898316
Arber <arber.abi1@outlook.com>
2024-11-25 13:40:28 +0100

    changed config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: false, // Optional: Set to true if you prefer URLs to always end with a slash
+  reactStrictMode: true, // Optional: Enables React's Strict Mode
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://www.mahmuti.com",
+        permanent: false, // Set to false if you expect this to change in the future
+      },
+    ];
+  },
 };
 
 export default nextConfig;
